da1df8a503b8c1fd102f82b925b37dce8b287f6a
simon-an <simon-an@users.noreply.github.com>
2019-11-13 15:42:21 +0100

    chore: fix unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ngx-safe');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngx-safe app is running!');
-  });
 });
